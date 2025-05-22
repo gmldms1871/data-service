e7422ac675c2b10a3414db88c2153e59090f1428
gmldms1871 <gmldms1871@hanyang.ac.kr>
2025-05-22 20:00:15 +0900

    머지

---
+++ backend/src/main/java/com/example/demo/mapper/TransactionMapper.java
@@ -0,0 +1,12 @@
+package com.example.demo.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
+
+@Mapper
+public interface TransactionMapper {
+    Integer getSellerConfirmed(@Param("transactionId") String transactionId);  // 판매자 거래 확인
+    Integer getBuyerConfirmed(@Param("transactionId") String transactionId);   // 구매자 거래 확인
+}
